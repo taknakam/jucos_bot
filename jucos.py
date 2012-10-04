f40f951f249a2fcbd2e5b2c79ae080f3927e0dc1
taknakam <charlo10@gmail.com>
2012-10-04 16:31:11 +0900

    create version20121004

--- jucos.py
+++ jucos.py
@@ -16,26 +16,51 @@ API = twitter.Api(
     access_token_secret=OAUTH_TOKEN_SECRET
 )
 
-def reply_tweet():
-    newest_id = API.GetUserTimeline()[0].id
-    tl = API.GetFriendsTimeline(since_id=newest_id)
-    # newest_id = max(tl, key=lambda x: x.id).id
+def jucos_pattern(tl):
+    """
+    Yield tweets that are not posted by 'jucos_bot'
+    and contain 'ジャコス' or 'ジャスコ'.
+    """
     for tweet in tl:
         if ((tweet.user.screen_name != 'jucos_bot') and
             (u'ジャコス' in tweet.text or u'ジャスコ' in tweet.text)):
-            reply = u'@' + tweet.user.screen_name + u' ジャコス行くの！！？'
-            print reply
-            try:
-                API.PostUpdate(reply, in_reply_to_status_id=tweet.id)
-            except twitter.TwitterError:
-                pass             
-        
+            yield tweet
+
+def reply_pattern(tl):
+    """
+    Yield tweets that are not posted by 'jucos_bot'
+    and contain '@jucos_bot'
+    """
+    for tweet in tl:
+        if ((tweet.user.screen_name != 'jucos_bot') and
+            (u'@jucos_bot' in tweet.text)):
+            yield tweet
+
+def reply_tweet():
+    newest_id = API.GetUserTimeline()[0].id
+    # newest_id = max(tl, key=lambda x: x.id).id
+    tl = API.GetFriendsTimeline(since_id=newest_id)
+    for tweet in reply_pattern(tl):
+        reply = u'@' + tweet.user.screen_name + u' ええー……っ'
+        print reply
+        try:
+            API.PostUpdate(reply, in_reply_to_status_id=tweet.id)
+        except twitter.TwitterError:
+            pass
+    for tweet in jucos_pattern(tl):
+        reply = u'@' + tweet.user.screen_name + u' ジャコス行くの！！？'
+        print reply
+        try:
+            API.PostUpdate(reply, in_reply_to_status_id=tweet.id)
+        except twitter.TwitterError:
+            pass
+
 def post_tweet(listin):
     if random.random() < 0.05:
         post = random.choice(listin)
         print post
         API.PostUpdate(post)
-    
+
 FILENAME = 'tweet.txt'
 
 tweet_list = [line.strip() for line in open(FILENAME)]
